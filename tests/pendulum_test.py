14271fa74121c7f02a51d7b0a1e16732b695b14d
villinv <vvillin@hotmail.fr>
2024-12-10 13:35:35 +0100

    simpler

--- tests/pendulum_test.py
+++ tests/pendulum_test.py
@@ -102,5 +102,5 @@ def main(_config):
 
     from polaris.trainers.sync_trainer import SynchronousTrainer
 
-    trainer = SynchronousTrainer(config, restore=config["restore"])
+    trainer = SynchronousTrainer(config, restore=config["restore"], with_spectator=False)
     trainer.run()
